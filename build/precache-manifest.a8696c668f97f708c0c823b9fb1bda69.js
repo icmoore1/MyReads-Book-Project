53f50c8325656501d831b06e3cf2e55189f17787
Isaac Moore <icmoore1@bellsouth.net>
2019-02-21 13:46:49 -0500

    initial commit

---
+++ build/precache-manifest.a8696c668f97f708c0c823b9fb1bda69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae804ad878f4cea83a26",
+    "url": "/static/js/main.ae804ad8.chunk.js"
+  },
+  {
+    "revision": "ef4727344a9b97631194",
+    "url": "/static/js/1.ef472734.chunk.js"
+  },
+  {
+    "revision": "ae804ad878f4cea83a26",
+    "url": "/static/css/main.883f45e2.chunk.css"
+  },
+  {
+    "revision": "61276f5070efdac7f76a916d3da72cb3",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/Shelf.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Book from './Book';
+
+
+
+class Shelf extends React.Component {
+//  componentDidMount() {
+//    console.log(this);
+//  }
+   render() {
+      return (
+       <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              this.props.book.map((book, key) => <Book bookUpdater={this.props.bookUpdater}  book={book} key={key} />)
+            }
+          </ol>
+        </div>
+      </div>
+      );
+   }
+}
+
+
+
+
+export default Shelf;
